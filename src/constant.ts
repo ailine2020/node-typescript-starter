2f58cd78e94e372c3e711c16fcc28cfa3987f6eb
Ailine <ailine.soto@outlook.fr>
2021-05-14 12:36:32 +0200

    Use Cases category + fix bug

--- src/constant.ts
+++ src/constant.ts
@@ -3,7 +3,7 @@ require("dotenv").config();
 //NODE_ENV = production
 //NODE_ENV = development
 
-type nodeEnvType = "development" | "production";
+type nodeEnvType = "development" | "production"
 
 //General
 export const NODE_ENV = (process.env.NODE_ENV as nodeEnvType) || "development";
@@ -14,7 +14,7 @@ export const API_BASE_URL = process.env.API_BASE_URL || "/api/v1/";
 
 //Database credentials
 export const DB_DIALECT = process.env.DB_DIALECT || "mysql";
-export const DB_DATABASE = process.env.DB_DATABASE || "skilltree_dev";
+export const DB_DATABASE = process.env.DB_DATABASE || "skill_tree_dev";
 export const DB_USERNAME = process.env.DB_USERNAME || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_HOST = process.env.DB_HOST || "localhost";
